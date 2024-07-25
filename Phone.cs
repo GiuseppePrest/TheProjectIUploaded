69b177655e292e2a6ac21e24d3fce130225aeba7
GiuseppePrest <106541500+GiuseppePrest@users.noreply.github.com>
2024-07-25 15:41:53 +0100

    Add files via upload

---
+++ Phone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectToUploadOnGit
+{
+    internal class Phone
+    {
+        public void phoneType()
+        {
+            Console.WriteLine("Your Phone is an Iphone");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectToUploadOnGit
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Your Phone is an Iphone");
+
+            Phone phone = new Phone();
+            phone.phoneType();
+        }
+         
+    }
+}
+
